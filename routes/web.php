93260dfbe294409b7a536d00ee8069d147c6e695
Sergio Alejandro Amaya <zsergioalejandro27@gmail.com>
2025-09-04 10:55:56 -0500

    Variables y tipos de variables

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,18 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+
+    echo "***** VARIABLES Y TIPOS DE VARIABLES ***** <br><br>";
+
+    $name = "Sergio Alejandro";
+
+    echo $name;
+
+    $age = rand(18,40); //Variable tipo integer "Random"
+    $height = 1.78; //Variable tipo float o decimal
+
+    $isLogin = true; //Variable tipo booleano
+
+    echo "<br>";
+    echo "Mi nombre es $name , tengo $age años y mido $height";
 });
