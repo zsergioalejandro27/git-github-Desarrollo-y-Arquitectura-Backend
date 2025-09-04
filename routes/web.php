04ea45793ed364f76c1e5659560b3f61c13d0306
Sergio Alejandro Amaya <zsergioalejandro27@gmail.com>
2025-09-04 11:33:59 -0500

    Estructura de datos y Bucles

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ Route::get('/', function () {
 
     echo $name;
 
-    $age = rand(18,40); //Variable tipo integer "Random"
+    $age = rand(18, 40); //Variable tipo integer "Random"
     $height = 1.78; //Variable tipo float o decimal
 
     $isLogin = true; //Variable tipo booleano
@@ -18,41 +18,74 @@ Route::get('/', function () {
     echo "<br>";
     echo "Mi nombre es $name , tengo $age años y mido $height";
 
-        echo "***** ESTRUCTURAS DE CONTROL ***** <br><br>";
-        echo "<br>";
-        $message = "Hola soy $name, ";
-        if($age >= 18) {
-            $message .= "Eres mayor de edad";
-        } else if ($age > 50) {
-            $message .+ "Eres un adulto mayor";
-        }
-        else {
-            $message .= "Eres menor de edad";
-        }
+    echo "***** ESTRUCTURAS DE CONTROL ***** <br><br>";
+    echo "<br>";
+    $message = "Hola soy $name, ";
+    if ($age >= 18) {
+        $message .= "Eres mayor de edad";
+    } else if ($age > 50) {
+        $message . +"Eres un adulto mayor";
+    } else {
+        $message .= "Eres menor de edad";
+    }
 
-        $message .= " " .($isLogin? "Ya estas logeado" : "No estas logeado"). "<br>";
+    $message .= " " . ($isLogin ? "Ya estas logeado" : "No estas logeado") . "<br>";
 
-        echo $message;
+    echo $message;
 
-        echo "<br>";
-        echo "***** FUNCIONES ***** <br><br>";
+    echo "<br>";
+    echo "***** FUNCIONES ***** <br><br>";
 
-        echo printUser($name,$age);
+    echo printUser($name, $age);
 
-        echo "<br>";
-        printUserWithCallBack($name, $age, callable: function(){
-            echo "Esta es una función callback, saludos!!! <br>";
-        });
+    echo "<br>";
+    printUserWithCallBack($name, $age, callable: function () {
+        echo "Esta es una función callback, saludos!!! <br>";
+    });
+
+    echo "<br>***** LISTAS Y MAPAS ***** <br><br>";
+
+    $lista = ["Sergio", "Amaya", 30, true];
+
+    echo $lista[1];
+
+    $ListProduct = [
+        "Camiseta" => [
+            "name" => "Camisa",
+            "price" => 20.5,
+            "Stock" => 50,
+            "isActive" => true,
+        ],
+        "Pantalon" => [
+            "name" => "Pantalon",
+            "price" => 25.5,
+            "Stock" => 50,
+            "isActive" => true,
+        ]
+    ];
+
+    echo $ListProduct["Camiseta"]["Stock"];
+
+    echo "<br>***** BUCLES ***** <br><br>";
+
+    for($i = 0; $i< count($lista); $i++){
+        echo $lista[$i] . "<br>";
+    }
+    foreach ($ListProduct as $product){
+        echo $product["name"] . " - " . $product["price"] . "<br>";
+    }
+
 });
 
-function printUser(string $name, int $age){
+function printUser(string $name, int $age)
+{
     return "El nombre del usuario es $name y su edad es $age años <br>";
 }
 
 
-function printUserWithCallBack(string $name, int $age, callable $callable){
+function printUserWithCallBack(string $name, int $age, callable $callable)
+{
 
     echo "Soy $name y  tengo $age años <br>";
     $callable();
-
 }
